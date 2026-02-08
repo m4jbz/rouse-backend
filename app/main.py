92d53d2060a86286de88aadbbbfd307555793c2c
m4jbz <marcoajb05@proton.me>
2026-02-08 00:55:37 -0600

    chore: ruta inutil eliminada y .env subido para evitar futuros problemas

--- app/main.py
+++ app/main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
-
 from app.api.endpoints import api_router
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 app.include_router(api_router)
