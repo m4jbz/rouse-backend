2903d724ca041e1a69c9fe025c0133a655cd478e
m4jbz <marcoajb05@proton.me>
2026-04-26 12:38:03 -0600

    new times

--- app/core/config.py
+++ app/core/config.py
@@ -25,10 +25,10 @@ class Settings(BaseSettings):
     FRONTEND_HOST: str
 
     # Token expiracion (minutos)
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 600
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    EMAIL_TOKEN_EXPIRE_MINUTES: int = 200
-    RESET_TOKEN_EXPIRE_MINUTES: int = 100
+    EMAIL_TOKEN_EXPIRE_MINUTES: int = 10
+    RESET_TOKEN_EXPIRE_MINUTES: int = 10
 
 
 settings = Settings()
